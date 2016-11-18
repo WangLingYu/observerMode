47957d8d9cdaa52f74d84ad5e80b8c1aac5d2107
wang <374951963@qq.com>
2016-11-18 21:39:25 +0800

    修改测试消息

--- src/MyObservable.java
+++ src/MyObservable.java
@@ -9,8 +9,8 @@ public class MyObservable extends Observable {
     public void postMessage(String content) {
 
         setChanged();
-        notifyObservers(content);
         System.out.println("被观察者开始发送消息啦!");
+        notifyObservers(content);
     }
 
 }
